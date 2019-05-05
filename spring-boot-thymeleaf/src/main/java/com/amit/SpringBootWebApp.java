21b284ffb1759ecbbf73d997e7371db581a793b3
Amit Sharma <amit.sharma.dev@gmail.com>
2019-05-05 23:00:19 +0530

    Add files via upload

---
+++ spring-boot-thymeleaf/src/main/java/com/amit/SpringBootWebApp.java
@@ -0,0 +1,13 @@
+package com.amit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebApp.class, args);
+	}
+
+}
