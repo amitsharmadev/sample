d2afb5bd2b926e584887b536a266ec92ae3d9ab2
Amit Sharma <amit.sharma.dev@gmail.com>
2019-05-05 22:59:18 +0530

    Add files via upload

---
+++ spring-boot-mysql/src/main/java/com/amit/Book.java
@@ -0,0 +1,49 @@
+package com.amit;
+
+import javax.persistence.Entity;
+//import org.hibernate.dialect.My
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+
+    public Book() {
+    }
+
+    public Book(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    
+    public void setName(String name) {
+        this.name = name;
+       
+    }
+}
---
+++ spring-boot-mysql/src/main/java/com/amit/BookRepository.java
@@ -0,0 +1,11 @@
+package com.amit;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface BookRepository extends CrudRepository<Book, Long> {
+
+    List<Book> findByName(String name);
+
+}
