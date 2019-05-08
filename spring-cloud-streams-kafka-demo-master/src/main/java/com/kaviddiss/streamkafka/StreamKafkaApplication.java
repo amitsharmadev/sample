5fbf9c04f28afc13d7ed55d92a516e7cd8a5177a
Amit Sharma <amit.sharma.dev@gmail.com>
2019-05-08 11:15:55 +0530

    Add files via upload

---
+++ spring-cloud-streams-kafka-demo-master/src/main/java/com/kaviddiss/streamkafka/StreamKafkaApplication.java
@@ -0,0 +1,12 @@
+package com.kaviddiss.streamkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamKafkaApplication.class, args);
+    }
+}
